97e21b1b9bb83af5261df795c56d187049399a69
vihdutta <vihdutta@gmail.com>
2024-12-29 17:26:04 -0500

    implemented scaling and dragging of the gui grid

---
+++ helpers/spatial_hashing.py
@@ -0,0 +1,5 @@
+class SpatialHash:
+    def __init__(self, grid_size):
+        self.hash_table = {}
+
+    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from sim_objects import Entity, Coin
+from helpers.sim_objects import Entity, Coin
 
 pygame.init()
 pygame.display.set_caption("Game Optimization Simulation")
@@ -27,24 +27,43 @@ def draw_grid(screen, grid_size, cell_size):
 def run_simulation():
     WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+    scale_factor = 1.0
+    dragging = False
+    grid_x_offset = 0
+    grid_y_offset = 0
+    last_mouse_pos = (0, 0)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.VIDEORESIZE:
+            if event.type == pygame.VIDEORESIZE:
                 WINDOW_WIDTH, WINDOW_HEIGHT = event.w, event.h
                 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+            if event.type == pygame.MOUSEWHEEL:
+                scale_factor += event.y / 10
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    dragging = True
+                    last_mouse_pos = event.pos
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    dragging = False
+            if event.type == pygame.MOUSEMOTION:
+                if dragging:
+                    dx, dy = event.pos[0] - last_mouse_pos[0], event.pos[1] - last_mouse_pos[1]
+                    grid_x_offset += dx
+                    grid_y_offset += dy
+                    last_mouse_pos = event.pos
 
         # ensures the grid_size is an exact multiple
         # of cell_size so the cells aren't cut off
-        cell_pixel_size = int(WINDOW_HEIGHT * 0.8 // CELLS)
+        cell_pixel_size = max(1, int(WINDOW_HEIGHT * 0.8 // CELLS * scale_factor))
         grid_pixel_size = cell_pixel_size * CELLS
 
-        # center the grid
-        grid_x = (WINDOW_WIDTH - grid_pixel_size) // 2
-        grid_y = (WINDOW_HEIGHT - grid_pixel_size) // 2
+        grid_x = (WINDOW_WIDTH - grid_pixel_size) // 2 + grid_x_offset
+        grid_y = (WINDOW_HEIGHT - grid_pixel_size) // 2 + grid_y_offset
 
         screen.fill((255, 255, 255))
 
